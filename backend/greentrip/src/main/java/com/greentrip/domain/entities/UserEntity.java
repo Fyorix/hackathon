00a6dc7ab28be3eaba8a2833d81b7de195c80f5e
damien.lugagne-delpon <damien.lugagne-delpon@epita.fr>
2026-07-07 18:09:05 +0200

    feat: add properties to user

--- backend/greentrip/src/main/java/com/greentrip/domain/entities/UserEntity.java
+++ backend/greentrip/src/main/java/com/greentrip/domain/entities/UserEntity.java
@@ -1,6 +1,7 @@
 package com.greentrip.domain.entities;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public record UserEntity(
     Long id,
@@ -11,6 +12,13 @@ public record UserEntity(
     Integer carbonPointsBalance,
     Double totalCo2Saved,
     Double totalKm,
+    String stravaRefreshToken,
+    Double homeLat,
+    Double homeLng,
+    Double workLat,
+    Double workLng,
+    LocalTime workStartTime,
+    LocalTime workEndTime,
     Long companyId,
     LocalDateTime createdAt,
     Long stravaAthleteId,
--- backend/greentrip/src/main/java/com/greentrip/domain/models/UserModel.java
+++ backend/greentrip/src/main/java/com/greentrip/domain/models/UserModel.java
@@ -1,8 +1,16 @@
 package com.greentrip.domain.models;
 
-import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "USERS")
